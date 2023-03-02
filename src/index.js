023492a75b666cd2dde18d354d5e596cf47152a8
ismael strey pereira <ismaelstrey@hotmail.com>
2023-03-02 10:08:04 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import routes from "./routes";
 
-dotenv.config()
-const port = process.env.PORT || 3001
+dotenv.config();
+const port = process.env.PORT || 3001;
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-routes(app)
-app.listen(port, () => console.log(`http://localhost:${port}`))
+routes(app);
+app.listen({ host: process.env.HOST, port: process.env.PORT }, () =>
+    console.log(`${process.env.HOST}:${process.env.PORT}`)
+);
